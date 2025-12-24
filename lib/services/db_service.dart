d64ff3e79b5864413fbea74c8c7e2019e3dc4f14
siyamcst <saidurrahman1004@gmail.com>
2025-12-25 01:06:24 +0600

    get public post

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -8,21 +8,41 @@ import '../models/trip_model.dart';
 class DBService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-//Fetch Data
+  //Fetch Data
   Stream<List<TripModel>> getTripData(String uid) {
-    return _db.collection('trips').where('userId', isEqualTo: uid).orderBy(
-        'date', descending: true).limit(50).snapshots().map((snapshots){
-          return snapshots.docs.map((doc){
-            return TripModel.fromMap( doc.data(), doc.id);
+    return _db
+        .collection('trips')
+        .where('userId', isEqualTo: uid)
+        .orderBy('date', descending: true)
+        .limit(50)
+        .snapshots()
+        .map((snapshots) {
+          return snapshots.docs.map((doc) {
+            return TripModel.fromMap(doc.data(), doc.id);
           }).toList();
+        });
+  }
+
+  //Get New Public Post only
+  Stream<List<TripModel>> getPublicTripData(String uid) {
+    return _db
+        .collection('trips')
+        .where('isPublic', isEqualTo: true)
+        .orderBy('date', descending: true)
+        .limit(50)
+        .snapshots()
+        .map((snapshots) {
+      return snapshots.docs.map((doc) {
+        return TripModel.fromMap(doc.data(), doc.id);
+      }).toList();
     });
   }
 
+
   //Save or Put data
-  Future<void> saveTrip(TripModel tripModel) async{
+  Future<void> saveTrip(TripModel tripModel) async {
     await _db.collection('trips').doc(tripModel.id).set(tripModel.toMap());
   }
-
 
   //delet data
   Future<void> deletTrip(String tripId) async {
@@ -30,52 +50,51 @@ class DBService {
   }
 
   //Find Last Updated Post
-  Future<DateTime?> getLastUpdatedDate(String uid) async{
-    try{
-      final querySnapshot = await _db.collection('trips').where('userId' , isEqualTo: uid).orderBy(
-          'date',descending: true).limit(1).get();
+  Future<DateTime?> getLastUpdatedDate(String uid) async {
+    try {
+      final querySnapshot = await _db
+          .collection('trips')
+          .where('userId', isEqualTo: uid)
+          .orderBy('date', descending: true)
+          .limit(1)
+          .get();
 
-      if(querySnapshot.docs.isNotEmpty){
+      if (querySnapshot.docs.isNotEmpty) {
         return (querySnapshot.docs.first.data()['date'] as Timestamp).toDate();
-
       }
       return null;
-    }catch(e){
+    } catch (e) {
       print("Error getting last updated date: $e");
       return null;
-
     }
-
   }
 
   //Image Upload Function Or Image Storage //Not Firebase Storage
-  Future<String> uploadImage(File imageFile) async{
-    try{
+  Future<String> uploadImage(File imageFile) async {
+    try {
       //create request
-      var request = http.MultipartRequest('POST', Uri.parse('https://api.imgbb.com/1/upload?key=${AppConst.imageAPIKey}')); //create request
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse('https://api.imgbb.com/1/upload?key=${AppConst.imageAPIKey}'),
+      ); //create request
 
       // add image file to request //'image'feild of imgbb
-      request.files.add(await http.MultipartFile.fromPath('image', imageFile.path));
+      request.files.add(
+        await http.MultipartFile.fromPath('image', imageFile.path),
+      );
       //sent Request
       var response = await request.send();
 
       //if response seccess
-      if(response.statusCode == 200){
+      if (response.statusCode == 200) {
         var responseData = await response.stream.bytesToString();
         var jsonResponse = jsonDecode(responseData);
         return jsonResponse['data']['url']; //return image url Direct
-      }else{
+      } else {
         throw Exception('Failed to upload image');
       }
-
-
-    }catch(e){
+    } catch (e) {
       throw Exception(e.toString());
-
-
     }
-
   }
-
-
 }
